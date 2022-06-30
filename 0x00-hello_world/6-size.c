ebb71d43b6349d4147c79b561ef918b187417504
mulingwa <jacintasally@gmail.com>
2022-06-30 15:44:29 -0700

    still six

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
-#include <string.h>
 
 /**
-* main - Entry pint
-*
-*Return: Always 0 (Success)
-*/
+
+   * main - computes sizes of various data types
+
+   *
+
+   *Return: zero
+
+   */
+
 int main(void)
 
 {
 
-printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	char c;
 
-printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	int i;
 
-printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-
-printf("Size of a long long int: %lu byte(s)\n", sizeof(long long long int));
-
-printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	float f;
 
 
+
+	printf("Size of a char: %lu byte\(s\)\n", sizeof(c));
+
+	printf("Size of an int: %lu byte\(s\)\n", sizeof(i));
+
+	printf("Size of a long int: %lu byte\(s\)\n", sizeof(long int));
+
+	printf("Size of a long long int: %lu byte\(s\)\n", sizeof(long long int));
+
+	printf("Size of a float: %lu byte\(s\)\n", sizeof(f));
 
 	return (0);
 
