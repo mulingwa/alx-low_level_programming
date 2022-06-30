ed9a92567b8eea2d1fb9670aeb14934ab0870619
mulingwa <jacintasally@gmail.com>
2022-06-30 15:35:08 -0700

    last quiz updates

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,11 +13,9 @@ int main(void)
 {
 
 	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	size_t len=strlen(str);
+	size_t len = strlen(str);
 	/* Starting main */
-	
 	fwrite(str, len, 1, stderr);
-	
 	return (1);
 
 }
