ede1867fcdebf5e9d32a7137a5ef5fa9cd3aef39
mulingwa <jacintasally@gmail.com>
2022-07-05 09:16:35 -0700

    test 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - prints all the alphabets
+ *
+ */
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
