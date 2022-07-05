0af0183ffc3016b3aaa2eebe15fed95e9b908bfd
mulingwa <jacintasally@gmail.com>
2022-07-05 09:36:32 -0700

    test two

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+/**
+ * print_alphabet_x10 - prints lowercase letters ten times
+ */
+void print_alphabet_x10(void)
+{
+	int j;
+	int k;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
