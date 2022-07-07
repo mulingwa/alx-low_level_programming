7aa36aeab5335b6c36847dd5b61f40eb2d203df9
mulingwa <84895131+mulingwa@users.noreply.github.com>
2022-07-07 21:00:49 +0300

    Update 0-isupper.c
    betty changes

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 
-/*
+/**
  *isupper - check for uppercase letters
  *
  *return - (1) if uppercase or (0) if otherwise
@@ -10,8 +10,8 @@
 
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
-		return (1);
-	else
-		return (0);
+if ((c >= 'A') && (c <= 'Z'))
+return (1);
+else
+return (0);
 }
